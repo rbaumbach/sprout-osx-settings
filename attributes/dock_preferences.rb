d75f0b8ef390cfcc10f6c04cde0a7a9564ac2b39
rbaumbach <rbaumbach.github@gmail.com>
2015-03-02 21:19:08 -0800

    Fix dock_preferences attributes

--- attributes/dock_preferences.rb
+++ attributes/dock_preferences.rb
@@ -1,7 +1,7 @@
 # http://secrets.blacktree.com/?showapp=com.apple.dock
-node.default['sprout']['dock_preferences']= {
+node.default['sprout']['dock_preferences'] = {
   'orientation' => 'bottom',
   'auto_hide' => true,
-  'min_effect' => 'scale'
+  'min_effect' => 'scale',
   'tile_size' => 65,
 }
